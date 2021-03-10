29169d061f1a44d198db79ec991a0f645ffd5f99
Leo <leonardschiller420@gmail.com>
2021-03-10 22:38:55 +0100

    added .getMessage()

--- src/main/java/mappe/del1/hospital/HospitalClient.java
+++ src/main/java/mappe/del1/hospital/HospitalClient.java
@@ -12,7 +12,7 @@ public class HospitalClient {
         try{
             hospital.getDepartments().get(0).remove(patient2);
         }catch(RemoveException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
         System.out.println(hospital.getDepartments().get(0).patients.toString());
         hospital.getDepartments().get(0).addPatient(patient);
@@ -20,7 +20,7 @@ public class HospitalClient {
         try{
             hospital.getDepartments().get(0).remove(patient2);
         }catch(RemoveException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
         System.out.println(hospital.getDepartments().get(0).patients.toString());
     }
